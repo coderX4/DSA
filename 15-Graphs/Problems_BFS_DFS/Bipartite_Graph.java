2d5b01092d15c304edd850e29a29455ad9503496
coderX4 <104690831+coderX4@users.noreply.github.com>
2024-08-11 12:46:18 +0530

    Dsa

--- 15-Graphs/Problems_BFS_DFS/Bipartite_Graph.java
+++ 15-Graphs/Problems_BFS_DFS/Bipartite_Graph.java
@@ -11,13 +11,15 @@ public class Bipartite_Graph {
         color[start] = 1;
 
         while(!qu.isEmpty()){
-            int node = qu.poll();
+            int node = qu.peek();
+            qu.remove();
 
             for(Integer adjnode: graph.map.get(node)){
-                if(color[adjnode] == 0){
+                if(color[adjnode] == -1){
                     //toggle btw 0 and 1
                     // 1 ^ 1 = 0 && 1 ^ 0 = 1
                     color[adjnode] = 1 ^ color[node];
+                    qu.offer(adjnode);
                 }
                 else{
                     if(color[adjnode] == color[node]){
@@ -29,13 +31,37 @@ public class Bipartite_Graph {
         return true;
     }
 
+    static boolean DFS(int node,int col,int[] color,Graph<Integer> graph){
+        color[node] = col;
+        for(Integer adjnode: graph.map.get(node)){
+            if(color[adjnode] == -1){
+                if(!DFS(adjnode,1 ^ col,color,graph)){
+                    return false;
+                }
+            }
+            else{
+                if(color[adjnode] == col){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
     static  boolean Check_Bipartite(Graph<Integer> graph){
         int v = graph.map.size();
         int[] color = new int[v+1];
 
+        for(int i = 0;i <= v;i++){
+            color[i] = -1;
+        }
+
         for(int i = 1;i <= v;i++){
-            if(color[i] == 0){
-                if(!BFS(i, color, graph)){
+            if(color[i] == -1){
+//                if(!BFS(i, color, graph)){
+//                    return false;
+//                }
+                if(!DFS(i,1,color,graph)){
                     return false;
                 }
             }
@@ -45,12 +71,13 @@ public class Bipartite_Graph {
 
 
     public static void main(String[] args) {
-        //Integer[][] edges = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,8},{3,9},{9,10},{10,6}};
-        Integer[][] edges = {{1,2},{2,3},{3,4},{4,5},{5,6},{2,7},{7,8},{4,8}};
+        Integer[][] edges = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,8},{3,9},{9,10},{10,6}};
+        //Integer[][] edges = {{1,2},{2,3},{3,4},{4,5},{5,6},{2,7},{7,8},{4,8}};
         Graph<Integer> graph = new Graph<>(edges,true);
         graph.Print();
 
         boolean ans = Check_Bipartite(graph);
         System.out.println(ans);
+
     }
 }
--- 15-Graphs/Problems_BFS_DFS/Detect_Cycle_Directed_DFS.java
+++ 15-Graphs/Problems_BFS_DFS/Detect_Cycle_Directed_DFS.java
@@ -1,5 +1,53 @@
 package Problems_BFS_DFS;
 
 public class Detect_Cycle_Directed_DFS {
+    static boolean DFS_check(int node,int[] vis,int[] path,Graph<Integer> graph){
+        vis[node] = 1;
+        path[node] = 1;
 
+        for(Integer it: graph.map.get(node)){
+            if(vis[it] == 0){
+                if(graph.map.get(it) == null){
+                    return false;
+                }
+                if(DFS_check(it,vis,path,graph)){
+                    return true;
+                }
+            }
+            else{
+                if(path[it] == 1){
+                    return true;
+                }
+            }
+        }
+        path[node] = 0;
+        return false;
+    }
+
+    static boolean Check_cycle(int v,Graph<Integer> graph){
+        int[] vis = new int[v+1];
+        int[] path = new int[v+1];
+
+        for(int i = 1; i <= v; i++){
+            if(vis[i] == 0){
+                if(!graph.map.containsKey(i)){
+                    return false;
+                }
+                if(DFS_check(i,vis,path,graph)){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Integer[][] edges = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,8},{3,9},{9,10},{10,6}};
+        Integer[][] edges2 = {{1,2},{2,3},{3,4},{4,5},{5,6},{6,7},{7,8},{9,3},{10,9},{6,10}};
+        Graph<Integer> graph = new Graph<>(edges2,false);
+        graph.Print();
+
+        boolean ans = Check_cycle(10,graph);
+        System.out.println(ans);
+    }
 }
